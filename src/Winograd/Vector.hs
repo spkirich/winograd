89b31fc528314ff0ab197d6f53194635ffdfb3df
Sergey Kirichenko <spkirich@gmail.com>
2022-11-03 22:52:27 +0300

    Add Winograd.Vector module

---
+++ src/Winograd/Vector.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE GADTs, StandaloneDeriving, TemplateHaskell #-}
+
+-- | This module defines a non-empty type-safe vector.
+
+module Winograd.Vector
+  ( -- * Definition
+
+    Vector (..)
+
+  , S
+  , Z
+
+    -- * Construction
+
+  , literal
+
+    -- * Algebra
+
+  , add
+  , multiply
+
+    -- * Other functions
+
+  , zipWith
+
+  ) where
+
+import Data.Traversable
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+import Prelude hiding (zipWith)
+
+data Z
+data S n
+
+-- | A vector.
+data Vector n a where
+
+  -- | A singleton vector.
+  Singleton :: a -> Vector Z a
+
+  -- | A vector with an element prepended.
+  Extension :: a -> Vector n a -> Vector (S n) a
+
+deriving instance Eq   a => Eq   (Vector n a)
+deriving instance Show a => Show (Vector n a)
+
+instance Functor (Vector n) where
+  fmap = fmapDefault
+
+instance Foldable (Vector n) where
+  foldMap = foldMapDefault
+
+instance Traversable (Vector n) where
+  traverse f (Singleton x  ) = Singleton <$> f x
+  traverse f (Extension x v) = Extension <$> f x <*> traverse f v
+
+-- | A vector literal.
+literal :: Lift a => [a] -> Q Exp
+literal [       ] = error "the literal is empty"
+literal [ x     ] = [| Singleton x              |]
+literal ( x : v ) = [| Extension x $(literal v) |]
+
+-- | Zip two vectors together with a function.
+zipWith :: (a -> b -> c) -> Vector n a -> Vector n b -> Vector n c
+zipWith f (Singleton x  ) (Singleton y  ) = Singleton (f x y)
+zipWith f (Extension x u) (Extension y v) = Extension (f x y) $ zipWith f u v
+
+-- | Add two vectors.
+add :: Num a => Vector n a -> Vector n a -> Vector n a
+add = zipWith (+)
+
+-- | Multiply two vectors.
+multiply :: Num a => Vector n a -> Vector n a -> a
+multiply u = sum . zipWith (*) u
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,124 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, ScopedTypeVariables, TemplateHaskell #-}
+
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
-import Winograd
+import Test.QuickCheck
+
+import Winograd.Vector (Vector)
+import qualified Winograd.Vector as Vector
+
+instance Arbitrary a => Arbitrary (Vector Vector.Z a) where
+  arbitrary = Vector.Singleton <$> arbitrary
+
+instance (Arbitrary a, Arbitrary (Vector n a)) => Arbitrary (Vector (Vector.S n) a) where
+  arbitrary = Vector.Extension <$> arbitrary <*> arbitrary
+
+type Dim = Vector.S (Vector.S Vector.Z)
 
 spec :: Spec
-spec = undefined
+spec = do
+
+  describe "vector addition" $ do
+
+    prop "is commutative" $ \
+
+        (u :: Vector Dim Integer)
+        (v :: Vector Dim Integer)
+
+      -> let
+
+          l = u `Vector.add` v
+          r = v `Vector.add` u
+
+        in l == r
+
+    prop "is associative" $ \
+
+        (u :: Vector Dim Integer)
+        (v :: Vector Dim Integer)
+        (w :: Vector Dim Integer)
+
+      -> let
+
+          l = u `Vector.add` (v `Vector.add` w)
+          r = (u `Vector.add` v) `Vector.add` w
+
+        in l == r
+
+    prop "has a neutral element" $ \
+
+        (v :: Vector Dim Integer)
+
+      -> let
+
+          e = $(Vector.literal [0, 0, 0 :: Integer])
+
+          l = v `Vector.add` e
+          r = e `Vector.add` v
+
+        in l == v && r == v
+
+    prop "has a negative for every element" $ \
+    
+        (v :: Vector Dim Integer)
+  
+      -> let
+
+          n = negate <$> v
+
+          l = v `Vector.add` n
+          r = n `Vector.add` v
+
+          e = $(Vector.literal [0, 0, 0 :: Integer])
+
+        in l == e && r == e
+
+  describe "vector multiplication" $ do
+
+    prop "is commutative" $ \
+
+        (u :: Vector Dim Integer)
+        (v :: Vector Dim Integer)
+
+      -> let
+
+          l = u `Vector.multiply` v
+          r = v `Vector.multiply` u
+
+        in l == r
+
+    prop "is left distributive" $ \
+
+        (u :: Vector Dim Integer)
+        (v :: Vector Dim Integer)
+        (w :: Vector Dim Integer)
+
+      -> let
+
+          l = u `Vector.multiply` (v `Vector.add` w)
+          r = (u `Vector.multiply` v) + (u `Vector.multiply` w) 
+
+        in l == r
+
+    prop "is right distributive" $ \
+
+        (u :: Vector Dim Integer)
+        (v :: Vector Dim Integer)
+        (w :: Vector Dim Integer)
+
+      -> let
+
+          l = (u `Vector.add` v) `Vector.multiply` w
+          r = (u `Vector.multiply` w) + (v `Vector.multiply` w) 
+
+        in l == r
+
+    prop "is non-negative as a square" $ \
+
+        (v :: Vector Dim Integer)
+
+      -> v `Vector.multiply` v >= 0
 
 main :: IO ()
 main = hspec spec
